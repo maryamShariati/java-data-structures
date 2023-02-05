9c2cf711e601014347d44b7afbe05742db1410e4
maryamshariati <maryamshariati831@gmail.com>
2023-02-05 16:49:08 +0330

    complete implementation of method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import array.Array;
+import array.MyArray;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Array myArray = new MyArray(12);
+        myArray.add(2);
+        myArray.add(4);
+        myArray.add(6);
+        System.out.println(myArray.size());
+        myArray.remove(2);
+        myArray.remove(6);
+        System.out.println(myArray.size());
+
+
+
     }
 }
--- src/array/MyArray.java
+++ src/array/MyArray.java
@@ -1,38 +1,119 @@
 package array;
+import java.util.Arrays;
+
 
 public class MyArray implements Array{
+    private static final int[] emptyElementData = {};
+    transient int[] elementData;
+
+    private int numberIndex=0;
+
+    public MyArray(int initialCapacity)
+    {
+        if (initialCapacity > 0)
+        {
+            this.elementData = new int[initialCapacity];
+        }
+        else if (initialCapacity == 0)
+        {
+            this.elementData = emptyElementData;
+        }
+        else
+        {
+            throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
+        }
+    }
+
+
     @Override
     public void add(int element) {
+
+        ensureExplicitCapacity(numberIndex+1);
+        elementData[numberIndex] = element;
+        numberIndex++;
 
     }
 
     @Override
     public void remove(int element) {
 
+        int index = indexOf(element);
+        removeByIndex(index);
+
     }
 
     @Override
     public void removeByIndex(int index) {
 
+        for (int i = index; i < numberIndex; i++) {
+
+            elementData[i] = elementData[i+1];
+        }
+        numberIndex--;
     }
 
     @Override
     public int indexOf(int element) {
-        return 0;
+
+        int index = 0 ;
+
+        for (int e:elementData) {
+            if (element == e){
+
+                return index;
+            }
+          index++;
+        }
+
+        throw new IllegalArgumentException(" invalid input");
+
     }
 
     @Override
     public boolean contains(int element) {
+
+        for (int i = 0; i < numberIndex; i++) {
+
+            if (element == elementData[i]){
+                return true;
+            }
+        }
+
         return false;
+
     }
 
     @Override
     public int size() {
-        return 0;
+        return numberIndex;
     }
 
     @Override
     public void print() {
 
+        for (int e: elementData) {
+            System.out.println(e);
+        }
     }
+
+    private void ensureExplicitCapacity(int minCapacity) {
+        if (minCapacity - elementData.length > 0)
+            grow(minCapacity);
+    }
+
+    private void grow(int minCapacity)
+    {
+        int oldCapacity = elementData.length;
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+        if (newCapacity - minCapacity > 0)
+            newCapacity = minCapacity;
+
+       elementData = Arrays.copyOf(elementData , newCapacity);
+
+    }
+
+
+
+
+
 }
